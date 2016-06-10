47912f9962b472943d44e50d967c0630fe98d9cd
laushinka <laurie.malau@gmail.com>
2016-06-09 20:37:49 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# class takes word on initialization
+# match method that takes an array of possible anagrams
+  # - if no matches, return empty array
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |element|
+      @word.split("").sort == element.split("").sort
+    end  
+  end
+
+end
